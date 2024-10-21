15300b572ab915fb87184395e0c868eb592fd278
User Elkin <asaselok@mail.ru>
2024-10-21 16:30:01 +0300

    В одном месте тз "скорость >0 и ⩽250"  в другом " от 0 до 250", не знаю включает во втором 250 или нет, сделал по первому.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,31 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Car> carData = new ArrayList<>();
+        String confirmation = "Да";
+        String nameCar = "";
+        do {
+            if (nameCar.isEmpty()){
+                System.out.printf("Введите название %d машины!\n",carData.size()+1);
+                nameCar = sc.next();
+            }
+            System.out.printf("Введите скорость %d машины!Скорость должна быть целым числом больше нуля и не больше 250!\n",carData.size()+1);
+
+            if (sc.hasNextInt()) {
+                int speedCar = sc.nextInt();
+                if (speedCar > 0 && speedCar <= 250) {
+                    System.out.printf("Машина %s, скорость %d правильно?\nВведи [%s/Любой текст]\n", nameCar, speedCar, confirmation);
+                    if (sc.next().equalsIgnoreCase(confirmation)) {
+                        carData.add(new Car(nameCar, speedCar));
+                    }
+                    nameCar = "";
+                }
+            } else sc.next();
+
+        } while (carData.size() != 3);
+        System.out.println("Самая быстрая машина: "+ Race.whoIsFaster(carData));
     }
 }
